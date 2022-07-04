2a00b25155b0bbf967afa1a486b721cd3b9e1907
mmmrojas1993 <lgc.marm@gmail.com>
2022-07-04 03:09:38 +0000

    code terminado

--- src/main.py
+++ src/main.py
@@ -9,12 +9,14 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+from models import Person, Ship, Planet
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -30,15 +32,186 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+#User
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route("/users", methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users = list(map(lambda ship: ship.to_dict(), users))
+    return jsonify(users), 200
 
-    return jsonify(response_body), 200
+@app.route("/users/<int:user_id>/favorites", methods=['GET'])
+def get_user_favorites(user_id):
+    user = User.query.get(user_id)
+    people = user.get_people()
+    planets = user.get_planets()
+    ships = user.get_ships()
 
+    return jsonify({
+        "people": people,
+        "planets": planets,
+        "ships": ships
+    }), 200
+
+@app.route("/users", methods=['POST'])
+def create_user():
+    user = User()
+    user.email = request.json.get('email')
+    user.password = request.json.get('password')
+    user.save()
+    
+    return jsonify(user.to_dict()), 201
+
+@app.route("/users/<int:user_id>/favorites_planets", methods=['POST'])
+def create_favorite_planet(user_id):
+    favorite_planet = FavoritePlanet()
+    favorite_planet.user_id = user_id
+    favorite_planet.planet_id = request.json.get('planet_id')
+    favorite_planet.save()
+    
+    return jsonify(favorite_planet.to_dict()), 201
+
+@app.route("/users/<int:user_id>/favorites_ships", methods=['POST'])
+def create_favorite_ship(user_id):
+    favorite_ship = FavoriteShip()
+    favorite_ship.user_id = user_id
+    favorite_ship.ship_id = request.json.get('ship_id')
+    favorite_ship.save()
+    
+    return jsonify(favorite_ship.to_dict()), 201
+
+@app.route("/users/<int:user_id>/favorites_people/<int:favorite_person_id>", methods=['DELETE'])
+def delete_favorite_person(user_id, favorite_person_id):
+    favorite_person = FavoritePerson.query.get(favorite_person_id)
+    favorite_person.delete()
+
+    return jsonify(favorite_person.to_dict()), 201
+
+@app.route("/users/<int:user_id>/favorites_planets/<int:favorite_planet_id>", methods=['DELETE'])
+def delete_favorite_planet(user_id, favorite_planet_id):
+    favorite_planet = FavoritePlanet.query.get(favorite_planet_id)
+    favorite_planet.delete()
+
+    return jsonify(favorite_planet.to_dict()), 201
+
+@app.route("/users/<int:user_id>/favorites_ships/<int:favorite_ship_id>", methods=['DELETE'])
+def delete_favorite_ship(user_id, favorite_ship_id):
+    favorite_ship = FavoriteShip.query.get(favorite_ship_id)
+    favorite_ship.delete()
+
+    return jsonify(favorite_ship.to_dict()), 201
+
+#People
+
+@app.route("/people", methods=['GET'])
+def getPeople():
+    people = Person.query.all()
+    people = list(map(lambda person: person.to_dict(), people))
+    return jsonify(people), 200
+
+@app.route("/people/<int:person_id>", methods = ["GET"])
+def getPerson(person_id):
+    person = Person.query.get(person_id)
+    
+    if person:
+        return jsonify(person.to_dict())
+
+    return jsonify({"message": "Person not found"})
+
+@app.route("/users/<int:user_id>/favorites_people", methods=['POST'])
+def create_favorite_person(user_id):
+    favorite_person = FavoritePerson()
+    favorite_person.user_id = user_id
+    favorite_person.person_id = request.json.get('person_id')
+    favorite_person.save()
+    
+    return jsonify(favorite_person.to_dict()), 201
+
+@app.route("/people", methods=['POST'])
+def createPerson():
+    person = Person()
+    person.name = request.json.get('name')
+    person.height = request.json.get('height')
+    person.mass = request.json.get('mass')
+    person.hair_color = request.json.get('hair_color')
+    person.skin_color = request.json.get('skin_color')
+    person.eye_color = request.json.get('eye_color')
+    person.birth_year = request.json.get('birth_year')
+    person.gender = request.json.get('gender')
+    person.save()
+    
+    return jsonify(person.to_dict()), 201
+
+#Planets
+
+@app.route("/planets", methods=['GET'])
+def getPlanets():
+    planets = Planet.query.all()
+    planets = list(map(lambda planet: planet.to_dict(), planets))
+    return jsonify(planets), 200
+
+@app.route("/planets/<int:planet_id>", methods = ["GET"])
+def getPlanet(planet_id):
+    planet = Planet.query.get(planet_id)
+    
+    if planet:
+        return jsonify(planet.to_dict())
+
+    return jsonify({"message": "Planet not found"})
+
+@app.route("/planets", methods=['POST'])
+def createPlanet():
+    planet = Planet()
+    planet.name = request.json.get('name')
+    planet.rotation_period = request.json.get('rotation_period')
+    planet.orbital_period = request.json.get('orbital_period')
+    planet.diameter = request.json.get('diameter')
+    planet.climate = request.json.get('climate')
+    planet.gravity = request.json.get('gravity')
+    planet.terrain = request.json.get('terrain')
+    planet.surface_water = request.json.get('surface_water')
+    planet.population = request.json.get('population')
+    planet.save()
+    
+    return jsonify(planet.to_dict()), 201
+
+# Ships
+
+@app.route("/ships", methods=['GET'])
+def getShips():
+    ships = Ship.query.all()
+    ships = list(map(lambda ship: ship.to_dict(), ships))
+    return jsonify(ships), 200
+
+@app.route("/ships/<int:ship_id>", methods = ["GET"])
+def getShip(ship_id):
+    ship = Ship.query.get(ship_id)
+    
+    if ship:
+        return jsonify(ship.to_dict())
+
+    return jsonify({"message": "Ship not found"})
+
+@app.route("/ships", methods=['POST'])
+def createShip():
+    ship = Ship()
+    ship.name = request.json.get('name')
+    ship.model = request.json.get('model')
+    ship.manufacturer = request.json.get('manufacturer')
+    ship.cost_in_credits = request.json.get('cost_in_credits')
+    ship.length = request.json.get('length')
+    ship.max_atmosphering_speed = request.json.get('max_atmosphering_speed')
+    ship.crew = request.json.get('crew')
+    ship.passengers = request.json.get('passengers')
+    ship.cargo_capacity = request.json.get('cargo_capacity')
+    ship.consumables = request.json.get('consumables')
+    ship.hyperdrive_rating = request.json.get('hyperdrive_rating')
+    ship.MGLT = request.json.get('MGLT')
+    ship.starship_class = request.json.get('starship_class')
+    ship.save()
+    
+    return jsonify(ship.to_dict()), 201
+    
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -2,18 +2,233 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(250))
+    email = db.Column(db.String(250))
+    people = db.relationship('Person', secondary='favorites_people', backref='users')
+    planets = db.relationship('Planet', secondary='favorites_planets', backref='users')
+    ships = db.relationship('Ship', secondary='favorites_ships', backref='users')
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def save(self):
+        db.session.add(self)  # INSERT
+        db.session.commit()  # Guarda el INSERT
 
-    def serialize(self):
+    def update(self):
+        db.session.commit()  # Guarda el UPDATE
+
+    def delete(self):
+        db.session.delete(self)  # DELETE
+        db.session.commit()  # Guarda el DELETE
+
+    def get_people(self):
+        return list(map(lambda person: person.to_dict(), self.people))
+    
+    def get_planets(self):
+        return list(map(lambda planet: planet.to_dict(), self.planets))
+    
+    def get_ships(self):
+        return list(map(lambda ship: ship.to_dict(), self.ships))
+
+    def to_dict(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "password": self.password,
+            "email": self.email
+        }
+
+class Person(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    height = db.Column(db.String(250))
+    mass = db.Column(db.String(250))
+    hair_color = db.Column(db.String(250))
+    skin_color = db.Column(db.String(250))
+    eye_color = db.Column(db.String(250))
+    birth_year = db.Column(db.String(250))
+    gender = db.Column(db.String(250))
+
+    def save(self):
+        db.session.add(self)  # INSERT
+        db.session.commit()  # Guarda el INSERT
+
+    def update(self):
+        db.session.commit()  # Guarda el UPDATE
+
+    def delete(self):
+        db.session.delete(self)  # DELETE
+        db.session.commit()  # Guarda el DELETE
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender
+        }
+        
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    rotation_period = db.Column(db.String(250))
+    orbital_period = db.Column(db.String(250))
+    diameter = db.Column(db.String(250))
+    climate = db.Column(db.String(250))
+    gravity = db.Column(db.String(250)) 
+    terrain = db.Column(db.String(250))
+    surface_water = db.Column(db.String(250))
+    population = db.Column(db.String(250))
+    
+
+    def save(self):
+        db.session.add(self)  # INSERT
+        db.session.commit()  # Guarda el INSERT
+
+    def update(self):
+        db.session.commit()  # Guarda el UPDATE
+
+    def delete(self):
+        db.session.delete(self)  # DELETE
+        db.session.commit()  # Guarda el DELETE
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "population": self.population
+        }
+
+class Ship(db.Model):
+    __tablename__ = 'ships'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    model = db.Column(db.String(250))
+    manufacturer = db.Column(db.String(250))
+    cost_in_credits = db.Column(db.String(250))
+    length = db.Column(db.String(250))
+    max_atmosphering_speed = db.Column(db.String(250))
+    crew = db.Column(db.String(250))
+    passengers = db.Column(db.String(250))
+    cargo_capacity = db.Column(db.String(250))
+    consumables = db.Column(db.String(250))
+    hyperdrive_rating = db.Column(db.String(250))
+    MGLT = db.Column(db.String(250))
+    starship_class = db.Column(db.String(250))
+    
+
+    def save(self):
+        db.session.add(self)  # INSERT
+        db.session.commit()  # Guarda el INSERT
+
+    def update(self):
+        db.session.commit()  # Guarda el UPDATE
+
+    def delete(self):
+        db.session.delete(self)  # DELETE
+        db.session.commit()  # Guarda el DELETE
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "hyperdrive_rating": self.hyperdrive_rating,
+            "MGLT": self.MGLT,
+            "starship_class": self.starship_class
+        }
+
+class FavoritePerson(db.Model):
+    __tablename__ = 'favorites_people'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    person_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+
+    def save(self):
+        db.session.add(self)  # INSERT
+        db.session.commit()  # Guarda el INSERT
+
+    def update(self):
+        db.session.commit()  # Guarda el UPDATE
+
+    def delete(self):
+        db.session.delete(self)  # DELETE
+        db.session.commit()  # Guarda el DELETE
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "person_id": self.person_id
+        }
+
+class FavoritePlanet(db.Model):
+    __tablename__ = 'favorites_planets'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+    def save(self):
+        db.session.add(self)  # INSERT
+        db.session.commit()  # Guarda el INSERT
+
+    def update(self):
+        db.session.commit()  # Guarda el UPDATE
+
+    def delete(self):
+        db.session.delete(self)  # DELETE
+        db.session.commit()  # Guarda el DELETE
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id
+        }
+
+class FavoriteShip(db.Model):
+    __tablename__ = 'favorites_ships'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    ship_id = db.Column(db.Integer, db.ForeignKey('ships.id'))
+
+    def save(self):
+        db.session.add(self)  # INSERT
+        db.session.commit()  # Guarda el INSERT
+
+    def update(self):
+        db.session.commit()  # Guarda el UPDATE
+
+    def delete(self):
+        db.session.delete(self)  # DELETE
+        db.session.commit()  # Guarda el DELETE
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "ship_id": self.ship_id
         }
